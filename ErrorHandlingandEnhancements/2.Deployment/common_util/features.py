054e24bcea80f874d938072957c7c6877eb5b539
ARUN A MENON <52127560+arunamenon@users.noreply.github.com>
2021-12-18 21:17:20 +0530

    Add files via upload

--- ErrorHandlingandEnhancements/2.Deployment/common_util/features.py
+++ ErrorHandlingandEnhancements/2.Deployment/common_util/features.py
@@ -1,9 +1,21 @@
 import math
 
+
 #Write a command to create 5 the feature that create log(log_Peatal_Width)
 #Will need to retrain the model on 5 features and create new model.pkl file
 
-def process():
-    {
-
+def process(data):
+    feature_engineering_dict = {
+     'Add log of petal length' : 1
     }
+    
+    for u,v in feature_engineering_dict.items():
+        if v==1:
+            data = add_log_petal_len(data)
+    return data
+def add_log_petal_len(data):
+    if float(data['Petal_Length'])>0:
+        data['log(Petal_length)'] = math.log(float(data['Petal_Length']))
+    else:
+        data['log(Petal_length)'] = 0
+    return data
--- ErrorHandlingandEnhancements/2.Deployment/common_util/utility.py
+++ ErrorHandlingandEnhancements/2.Deployment/common_util/utility.py
@@ -2,7 +2,36 @@ from schema import Schema, And, Use, SchemaError
 
 #Write code here for exception handling
 
-def exceptions():
-    {
+import numbers
 
-    } 
+def exceptions(data):
+    checks_dict = {
+     'Numeric input required' : test_if_input_values_are_numeric(data),
+     #'Sum input nmeric values' : sum_numeric_values_in_input(data)
+     }
+    return checks_dict
+    
+def test_if_input_values_are_numeric(data):
+    list_input = [] 
+    for u,v in data.items():
+        try:
+            list_input += [float(v)]
+        except:
+            list_input += [v]
+    numeric_flags = [1 if isinstance(v, numbers.Number) else 0 for v in list_input]
+    non_numeric_input_features = []
+    count = 0
+    for i in numeric_flags:
+        if i==0:
+            non_numeric_input_features += [list(data.keys())[count]]
+        count += 1
+    if (len(non_numeric_input_features)==0) & (len(numeric_flags)>0):
+        #return True, 'All numeric values in input'
+        return True
+    else:
+        #return False, 'Provide numeric input values for ' + str(non_numeric_input_features)
+        return False
+    
+def sum_numeric_values_in_input(data):
+    return sum([v for u,v in data.items()])
+    
