f6365247add4e7fe86238b6cc8877a1982dc06eb
ARUN A MENON <52127560+arunamenon@users.noreply.github.com>
2021-12-18 21:17:42 +0530

    Add files via upload

--- ErrorHandlingandEnhancements/1.Development/config.py
+++ ErrorHandlingandEnhancements/1.Development/config.py
@@ -1,9 +1,10 @@
 #Contains all what is dynamic
 
-path = r"C:\Users\as14329\Desktop\FSDSessionAPI\5. S2Enhancements"
+path = r"C:/Users/am17801/Desktop/Projects/99 Miscellaneous/08 ADS/07 Full stack DS program/FSDSessionAPI-main/FSDSessionAPI/4. FSDSession1Assignment1/ErrorHandlingandEnhancements/"
 
 datapath = path + "\\1.Development\\traindata\iris.csv"
 modeloutput = path + "\\2.Deployment\\artifacts\model.pkl"
+modeloutput_5_features = path + "\\2.Deployment\\artifacts\model_5_features.pkl"
 rawfields = ["Sepal_Length", "Sepal_Width", "Petal_Length","Petal_Width"]
 target = "Class"
 testsize = 0.3
